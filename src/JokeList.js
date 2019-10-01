a0926b84a2069055ee79c6b4a83dbc523cdc8538
markswinimer <markswinimer@gmail.com>
2019-09-30 21:22:34 -0400

    initial commit

---
+++ src/JokeList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class JokeList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // STATE TBD
+    }
+  }
+  render() {
+    return(
+      <div>
+        <h1>Dad Jokes App</h1>
+      </div>
+    )
+  }
+}
+export default JokeList;
